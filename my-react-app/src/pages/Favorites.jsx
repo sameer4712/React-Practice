b20bdbf66b056e282642577315d77cad96d46ecb
sameer4712 <sameerptmc@gmail.com>
2025-08-03 15:42:28 +0530

    fav

--- my-react-app/src/pages/Favorites.jsx
+++ my-react-app/src/pages/Favorites.jsx
@@ -2,6 +2,7 @@ function Fav() {
     return (
         <div>
             <h3>No favorite yet</h3>
+            <h4>Pick your movies here</h4>
             <p>Add your favorite movies here</p>
         </div>
     )
