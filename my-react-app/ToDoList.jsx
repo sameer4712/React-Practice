5e58c9e2793cf705021faf5c47cc44691f976b7e
sameer4712 <sameerptmc@gmail.com>
2025-08-09 17:07:41 +0530

    To Do List

---
+++ my-react-app/ToDoList.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+
+function ToDoList() {
+    const [Task, SetTask] = useState(["Eat Dinner","Study Hard"])
+    const [NewTask, SetNewTask] = useState('')
+    function handleTask(event) {
+        SetNewTask(event.target.value)
+    }
+    function AddTask() {
+        if (NewTask.trim() !== "")
+            SetTask(t => [...t, NewTask]);
+        SetNewTask("")
+    }
+    function DeleteTask(index) {
+        const updatedTask = Task.filter((_, i) => i !== index)
+        SetTask(updatedTask)
+
+    }
+    function MoveUp(index) {
+        if (index > 0) {
+            const updatedTask = [...Task];
+            [updatedTask[index], updatedTask[index - 1]] = [updatedTask[index - 1], updatedTask[index]];
+            SetTask(updatedTask)
+        }
+
+    }
+    function MoveDown(index) {
+        if (index < Task.length - 1) {
+            const updatedTask = [...Task];
+            [updatedTask[index + 1], updatedTask[index]] = [updatedTask[index], updatedTask[index + 1]];
+            SetTask(updatedTask)
+        }
+
+    }
+    return (
+        <div className="main">
+            <div className=" todo" >
+                <h1>To Do List</h1>
+                <div className="item">
+                    <input type="text" placeholder="Enter a new Task......" value={NewTask} onChange={handleTask} />
+                    <button onClick={AddTask} className="addbutton"> Add</button>
+                </div>
+                <ol >
+                    {Task.map((item, index) =>
+                        <li key={index}>
+                            <span>{item}</span>
+                                <button onClick={() => DeleteTask(index)} className="delete">Delete</button>
+                                <button onClick={() => MoveUp(index)} className="moves">UP</button>
+                                <button onClick={() => MoveDown(index)} className="moves">DOWN</button>
+                        </li>
+                    )}
+                </ol>
+
+            </div>
+        </div>
+    )
+}
+
+export default ToDoList
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,21 @@
 import "./index.css"
-import Home from "./pages/Home"
-import { Routes, Route } from "react-router-dom"
-import Fav from "./pages/Favorites.jsx"
-import NavBar from "./components/NavBar.jsx"
+// import Home from "./pages/Home"
+// import { Routes, Route } from "react-router-dom"
+// import Fav from "./pages/Favorites.jsx"
+// import NavBar from "./components/NavBar.jsx"
+import ToDoList from "../ToDoList.jsx"
 
 function App() {
   return (
     <div>
-       <NavBar/>
-    <main className="main-content">
+       {/* <NavBar/> */}
+       <ToDoList />
+    {/* <main className="main-content">
       <Routes>
         < Route path="/" element={<Home />} />
         < Route path="/favorites" element={<Fav />} />
       </Routes>
-    </main>
+    </main> */}
     </div>
   )
 }
